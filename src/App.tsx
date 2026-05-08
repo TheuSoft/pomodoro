fbd67d92961c5ab7705b00d0d7df63998a6e6195
TheuSoft <matheusmarques150@hotmail.com>
2026-05-08 08:13:01 -0300

    Adicionando o Thema ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import './styles/theme.css';
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading> Ola Mundo </Heading>
+      <Heading> Ola Mundo 2 </Heading>
+      <Heading> Ola Mundo 1 </Heading>
+      <Heading> Ola Mundo 2 </Heading>
+      <Heading> Ola Mundo 3 </Heading>
+      <Heading> Ola Mundo 4 </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus nisi
         ipsam unde eum, nobis repudiandae odit quas. Quas laborum expedita in
