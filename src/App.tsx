e34d0bcb81019c9d60ea7739795b5b7721a52ee1
TheuSoft <matheusmarques150@hotmail.com>
2026-05-08 09:29:22 -0300

    Adicionando estruturacao de components do Heading e Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
@@ -5,18 +6,12 @@ import './styles/theme.css';
 export function App() {
   return (
     <>
-      <Heading> Ola Mundo </Heading>
-      <Heading> Ola Mundo 2 </Heading>
-      <Heading> Ola Mundo 1 </Heading>
-      <Heading> Ola Mundo 2 </Heading>
-      <Heading> Ola Mundo 3 </Heading>
-      <Heading> Ola Mundo 4 </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus nisi
-        ipsam unde eum, nobis repudiandae odit quas. Quas laborum expedita in
-        asperiores, delectus perferendis quasi facilis at, dolorum nulla
-        inventore.
-      </p>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+      <Container>
+        <Heading>Timer</Heading>
+      </Container>
     </>
   );
 }
