35041abbc395fa87a5fb9a0021a358f0edde5954
TheuSoft <matheusmarques150@hotmail.com>
2026-05-16 11:05:23 -0300

    feat: validando o formulario e criando uma nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,26 +1,73 @@
-import { PlayCircleIcon } from 'lucide-react';
 import { DefaultButton } from '../DefaultButton';
-import { Cycles } from '../Cycles';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/UseTaskContext';
+import { Cycles } from '../Cycles';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('DEU CERTO');
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // Conferir
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: '00:00', // Conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className='form'>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
+          labelText='task'
           id='meuInput'
           type='text'
-          labelText='Task'
-          placeholder='O que você vai fazer?'
+          placeholder='Digite algo'
+          ref={taskNameInput}
         />
       </div>
+
       <div className='formRow'>
-        <p>Lorem ipsum dolor sit amet consectetur,</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
+
       <div className='formRow'>
         <Cycles />
-        <p>000000</p>
       </div>
+
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} />
       </div>
